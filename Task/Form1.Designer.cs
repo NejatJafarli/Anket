6d04c821adbfeb98b5713cc72063b0965a46f3f9
Asus Rog Strix G17 <Asus Rog Strix G17@DESKTOP-O03QRDI>
2021-07-31 17:24:58 +0400

    Add project files.

---
+++ Task/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.txtJsonFileName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.btnDelete);
+            this.groupBox.Controls.Add(this.btnChange);
+            this.groupBox.Controls.Add(this.monthCalendar1);
+            this.groupBox.Controls.Add(this.btnAdd);
+            this.groupBox.Controls.Add(this.txtPhone);
+            this.groupBox.Controls.Add(this.label5);
+            this.groupBox.Controls.Add(this.label4);
+            this.groupBox.Controls.Add(this.label3);
+            this.groupBox.Controls.Add(this.txtEmail);
+            this.groupBox.Controls.Add(this.label2);
+            this.groupBox.Controls.Add(this.txtSurname);
+            this.groupBox.Controls.Add(this.label1);
+            this.groupBox.Controls.Add(this.txtName);
+            this.groupBox.Location = new System.Drawing.Point(13, 13);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(430, 442);
+            this.groupBox.TabIndex = 0;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Anket";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(12, 192);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 13;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(314, 255);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(110, 32);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "Elave Et";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(55, 124);
+            this.txtPhone.Mask = "(999) 00-000-00-00";
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(180, 26);
+            this.txtPhone.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(4, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Dogum";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(4, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tel";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(80, 90);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(155, 28);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Soyad";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSurname.Location = new System.Drawing.Point(88, 56);
+            this.txtSurname.Multiline = true;
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(155, 28);
+            this.txtSurname.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(50, 22);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(155, 28);
+            this.txtName.TabIndex = 0;
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.Location = new System.Drawing.Point(449, 11);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(155, 251);
+            this.ListBox.TabIndex = 1;
+            // 
+            // txtJsonFileName
+            // 
+            this.txtJsonFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtJsonFileName.Location = new System.Drawing.Point(449, 268);
+            this.txtJsonFileName.Multiline = true;
+            this.txtJsonFileName.Name = "txtJsonFileName";
+            this.txtJsonFileName.Size = new System.Drawing.Size(155, 28);
+            this.txtJsonFileName.TabIndex = 13;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(529, 302);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(77, 32);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(449, 302);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(77, 32);
+            this.btnLoad.TabIndex = 14;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChange.Location = new System.Drawing.Point(314, 217);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(110, 32);
+            this.btnChange.TabIndex = 14;
+            this.btnChange.Text = "Deyis";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Visible = false;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(314, 289);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(110, 32);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 467);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtJsonFileName);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.groupBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.MaskedTextBox txtPhone;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox ListBox;
+        private System.Windows.Forms.TextBox txtJsonFileName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Task/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task
+{
+    public partial class Form1 : Form
+    {
+        class User
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Email { get; set; }
+            public string Phone { get; set; }
+            public DateTime dateTime { get; set; }
+            public string FileName { get; set; }
+            public override string ToString()
+            {
+                return $"{Name}";
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            ListBox.SelectionMode = SelectionMode.MultiSimple;
+            Timer timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (ListBox.SelectedItems.Count > 0) btnDelete.Visible = true;
+            else btnDelete.Visible = false;
+            if (ListBox.SelectedItems.Count == 1)
+            {
+                btnChange.Visible = true;
+                btnChange.Location = btnAdd.Location;
+                btnAdd.Visible = false;
+            }
+            else
+            {
+                btnChange.Visible = false;
+                btnAdd.Visible = true;
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            User user = new User
+            {
+                Name = txtName.Text,
+                Surname = txtSurname.Text,
+                Email = txtEmail.Text,
+                Phone = txtPhone.Text,
+                dateTime = monthCalendar1.SelectionStart
+            };
+            ListBox.Items.Add(user);
+            ListBox.DisplayMember = "Name";
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtJsonFileName.Text) && ListBox.SelectedItems.Count > 0)
+            {
+                var list = ListBox.SelectedItems;
+                var TextJson = JsonSerializer.Serialize(list, new JsonSerializerOptions() { WriteIndented = true });
+                using (FileStream fs = new FileStream($"{txtJsonFileName.Text}.json", FileMode.Create))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs, Encoding.Unicode))
+                        sw.WriteLine(TextJson);
+                }
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (!TestFileName(txtJsonFileName.Text))
+            {
+                var text = File.ReadAllText($"{txtJsonFileName.Text}.json");
+                txtJsonFileName.Text = "";
+                var JsonUser = JsonSerializer.Deserialize<List<User>>(text);
+                foreach (var item in JsonUser)
+                    ListBox.Items.Add(item);
+            }
+        }
+        private bool TestFileName(in string FileName)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(".");
+            foreach (var item in directoryInfo.GetFiles())
+            {
+                if (item.Name == $"{FileName}.json")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            var ListUser = ListBox.SelectedItems[0] as User;
+            txtName.Text = ListUser.Name;
+            txtSurname.Text = ListUser.Surname;
+            txtEmail.Text = ListUser.Email;
+            txtPhone.Text = ListUser.Phone;
+            monthCalendar1.SetDate(ListUser.dateTime);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            while (ListBox.SelectedItems.Count > 0)
+                ListBox.Items.Remove(ListBox.SelectedItems[0]);
+        }
+    }
+}
